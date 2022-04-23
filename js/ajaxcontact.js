44144f7422049618fdd9bfb9e6116450e60a0f71
sky9820 <104008743+sky9820@users.noreply.github.com>
2022-04-23 16:44:13 +0530

    Updating JS files
    Updating JS files

--- js/ajaxcontact.js
+++ js/ajaxcontact.js
@@ -4,9 +4,14 @@ for our custom functions
 @author: Aakash
 ***********************/
 
+/**
+AJAX handler for form submission
+*/
 jQuery(document).ready(function (e) {
+	jQuery('.loader').hide();
 	jQuery('#postForm').on('submit',function(e) {
 		e.preventDefault();
+		jQuery('.loader').show();
 		var formData = new FormData(this);
 		formData.append('action', 'ajaxform_send_mail');
 		var ajaxurl = jQuery('#ajaxurl').val();
@@ -20,14 +25,15 @@ jQuery(document).ready(function (e) {
 			dataType: 'html',
 			success:function(data){
 				console.log("success");
-				//alert('OK');
 				var id = '#ajax_response';
 				jQuery(id).html('');
 				jQuery(id).append(data);
 				jQuery(id).show();
+				jQuery('.loader').hide();
 			},
 			error: function(MLHttpRequest, textStatus, errorThrown){
 				alert(errorThrown);
+				jQuery('.loader').hide();
 			}
 		});
 	});
@@ -38,38 +44,6 @@ jQuery("#post_image").on("change", function() {
 });
 
 
-/**
-AJAX handler for form submission
-*/
-function ajaxformsendmail(post_title, post_description, post_excerpt, thumbnail)
-{
-	console.log(thumbnail);
-	alert('fdfd');
-	
-	jQuery.ajax({
-		type: 'POST',
-		url: form_ajax.ajaxurl,
-		data: {
-		action: 'ajaxform_send_mail',
-		title: post_title,
-		description: post_description,
-		excerpt: post_excerpt,
-		thumbnail: thumbnail
-	},
-	success:function(data, textStatus, XMLHttpRequest){
-		alert('OK');
-		console.log(data);
-		var id = '#ajax_response';
-		jQuery(id).html('');
-		jQuery(id).append(data);
-	},
-	error: function(MLHttpRequest, textStatus, errorThrown){
-		alert(errorThrown);
-	}
-
-	});
-
-}
 
 /**
 Get Image URL and set in a hidden field
